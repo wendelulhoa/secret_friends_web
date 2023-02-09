52d15b2dbb6b11f74c0af04968ca1e79a27de04e
Wendel Ulhoa <wendelulhoa@gmail.com>
2023-02-08 21:27:25 -0300

    m

--- public/OneSignalSDKWorker.js
+++ public/OneSignalSDKWorker.js
@@ -1,3 +1,3 @@
-if( 'undefined' === typeof window){
-    importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
+if('undefined' === typeof window){
+    // importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
  }
